f6a80a09472f0bf20cfc21110a5b6db042b4c44f
aswin chandran <aswin1998ply@gmail.com>
2023-09-16 00:17:15 +0530

    table

--- script.js
+++ script.js
@@ -4,40 +4,44 @@ function change() {
   var xhttp = new XMLHttpRequest();
   var txt = document.getElementById("txt");
 
+
+
+
   //step 4
 
-  let promise = new Promise(function (resolve, reject) {
+ 
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         let output = JSON.parse(this.responseText);
         let val = "";
+     
+       
 
         for (let x = 0; x < output.length; x++) {
-          val += `<table border='1'>
+          var y = document.createElement("INPUT");
+          y.setAttribute("type", "checkbox");
+        
+          
+          val += `<table >
           <br>
           <tr >
-          <td>
-          &nbsp; &nbsp; ${output[x].id} &nbsp;
-          </td>
-          <td>
-          ${output[x].title} &nbsp; &nbsp;
-          </td>
-          <td>
-         <input type="checkbox"> &nbsp; &nbsp;
-          </td>
+          
+          <td>${output[x].id} </td>
+         
+         
+          <td>${output[x].title} </td>
+          <td><input type="checkbox"></td>
+       
        
         
           </tr>
           </table>`;
-          if (output[x].title == true) {
-            console.log("true");
-          }
-
+        
           txt.innerHTML = val;
         }
       }
     };
-  });
+ 
   //step 2
   xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
 
